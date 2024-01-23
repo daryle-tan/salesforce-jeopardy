e15ab9d016ad78f691ed9c19cb755b0ca54431d0
daryle <daryle.tan@gmail.com>
2024-01-22 20:46:00 -0600

    cleared App.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,69 +1,27 @@
-import React, { Component } from 'react';
-import { categories } from '../../testdata';
-import Gameboard from './Gameboard'
-import Scoreboard from './Scoreboard';
-import Response from './Response'
+import useState from "react"
+import { categories } from "../../testdata"
+import Gameboard from "./Gameboard"
+import Scoreboard from "./Scoreboard"
+import Response from "./Response"
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+export default function App() {
+  const [gameState, setGameState] = useState({
+    categories,
+    currentQuestion: {},
+    answeredQuestions: [],
+    score: 0,
+  })
 
-    this.state = {
-      categories,
-      currentQuestion: {},
-      answeredQuestions: [],
-      score: 0, 
-    };
-    this.handleResponse = this.handleResponse.bind(this)
-    this.selectQuestion = this.selectQuestion.bind(this)
-  }
-
-  componentDidMount() {
-    // Getting data from an external API
-    //1. A query to /api/categories to get a set of categories
-    //2. A set of queries afterwards to /api/category at each category id to get clues for that category
-  }
-
-  handleResponse(userResponse) {
-    const correctAnswer = this.state.currentQuestion.answer;
-    console.log(correctAnswer)
-    if (userResponse === correctAnswer) {
-      this.setState((state) => ({
-        score: state.score + state.currentQuestion.value,
-      }))
-    } else {
-      this.setState((state) => ({
-        currentQuestion: {},
-        score: state.score - state.currentQuestion.value,
-      }))
-    }
-    this.setState((state) => ({
-      currentQuestion: {},
-      answeredQuestions: state.answeredQuestions.concat(
-        state.currentQuestion)
-    }))
-  }
-
-  selectQuestion(question) {
-    console.log(question)
-    this.setState({
-      currentQuestion: question,
-      // answeredQuestions: this.state.answeredQuestions.concat(question)
-    })
-  }
-
-  render() {
-    return (
-      <div id="app">
-        <Gameboard
-          categories={this.state.categories}
-          answeredQuestions={this.state.answeredQuestions}
-          currentQuestion={this.state.currentQuestion}
-          selectQuestion={this.selectQuestion}
-        />
-        <Scoreboard score={this.state.score}/>
-        <Response submitResponse={this.handleResponse} /> 
-      </div>
-    );
-  }
+  return (
+    <div id="app">
+      <Gameboard
+        categories={this.state.categories}
+        answeredQuestions={this.state.answeredQuestions}
+        currentQuestion={this.state.currentQuestion}
+        selectQuestion={this.selectQuestion}
+      />
+      <Scoreboard score={this.state.score} />
+      <Response submitResponse={this.handleResponse} />
+    </div>
+  )
 }

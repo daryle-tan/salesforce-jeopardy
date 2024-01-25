bed4b9d14d8d07529371172290e804b7a1ba4fb4
daryle <daryle.tan@gmail.com>
2024-01-24 23:09:22 -0600

    added grid styling to gameboard and static content to categories

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -4,21 +4,20 @@ import Categories from "./Categories.js"
 const Gameboard = (props) => {
   return (
     <div
-      data-testid="gameboard"
-      id={props.currentQuestion.question ? "question" : "gameboard"}
+      id="gameboard"
+      // id={props.currentQuestion.question ? "question" : "gameboard"}
     >
-      {props.currentQuestion.question ? (
+      {/* {props.currentQuestion.question ? (
         <div>{props.currentQuestion.question}</div>
-      ) : (
-        <Categories
-          categories={props.categories}
-          selectQuestion={props.selectQuestion}
-        />
-      )}
+      ) : ( */}
+      <Categories
+        categories={props.categories}
+        selectQuestion={props.selectQuestion}
+      />
+      {/* )}  */}
       {/* was a question clicked?  */}
       {/* Yes? Show clue */}
       {/* No? Show Categories */}
-      Hi
     </div>
   )
 }

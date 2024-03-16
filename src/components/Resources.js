9b6a904be83f72341fa0cd9ca4ff4f81d533ae6d
daryle <daryle.tan@gmail.com>
2024-03-15 22:33:35 -0500

    added player's score to the Resources component

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -9,6 +9,7 @@ const Resources = ({ score }) => {
     <>
       {score < 15750 ? (
         <div>
+          <p>Your Score: {((score / 21000) * 100).toFixed(0)}%</p>
           <p>
             Nice try but you'll need to keep working. Repetition is mother of
             all learning. Here are more resources to help you successful pass
@@ -18,6 +19,7 @@ const Resources = ({ score }) => {
         </div>
       ) : score < 17850 ? (
         <div>
+          <p>Your Score: {((score / 21000) * 100).toFixed(0)}%</p>
           <p>
             Good Job! You're on track to successfully pass the exam! Here are
             some resources to help you continue to grow your knowledge and
@@ -27,6 +29,7 @@ const Resources = ({ score }) => {
         </div>
       ) : (
         <div>
+          <p>Your Score: {((score / 21000) * 100).toFixed(0)}%</p>
           <p>
             Great Job! Let's increase your probability of passing that exam.
             Here are some resources to help you continue to grow your knowledge

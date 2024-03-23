b61d72cbd63a698950f7eb2e2c15822d7af69e85
daryle <daryle.tan@gmail.com>
2024-03-22 23:53:11 -0500

    updated feedback with Contact Us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 function Footer() {
   return (
     <div id="footer" data-testid="response">
-      <a href="mailto:daryle.tan@gmail.com">Feedback</a>
+      <a href="mailto:daryle.tan@gmail.com">Contact Us</a>
     </div>
   )
 }

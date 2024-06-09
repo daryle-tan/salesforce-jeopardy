1978654034a8ee0f781d0282d6e7ddb8aaef1a49
daryle <daryle.tan@gmail.com>
2024-06-08 22:07:50 -0500

    added * to line 39

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, "build")))
 //   }
 // })
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 const pool = new Pool({

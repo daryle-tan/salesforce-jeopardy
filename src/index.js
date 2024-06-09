0ff001122dd0d6d1a595350314d5a5019a5c48fb
daryle <daryle.tan@gmail.com>
2024-06-09 01:10:54 -0500

    restructured index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@ import React from "react"
 import "./styles/styles.css"
 import App from "./components/App.js"
 import Contact from "./components/Contact.js"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom"
 import { createRoot } from "react-dom/client"
 
-const container = document.getElementById("root")
-const root = createRoot(container)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/contact" element={<Contact />} />
+    </Route>,
+  ),
+)
 
+const root = document.getElementById("root")
 root.render(
   <React.StrictMode>
-    <Router>
+    <RouterProvider router={router} />
+    {/* <Router>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </Router> */}
   </React.StrictMode>,
 )

131fa65a9779c9395f5c66d7a59cbc57e20b3fe2
daryle <daryle.tan@gmail.com>
2024-06-09 01:22:42 -0500

    removed router compoenents

--- src/index.js
+++ src/index.js
@@ -2,33 +2,19 @@ import React from "react"
 import "./styles/styles.css"
 import App from "./components/App.js"
 import Contact from "./components/Contact.js"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { createRoot } from "react-dom/client"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="*" element={<App />}>
-      <Route path="/contact" element={<Contact />} />
-    </Route>,
-  ),
-)
+const container = document.getElementById("root")
+const root = createRoot(container)
 
-const root = document.getElementById("root")
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <Router>
+    <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="*" element={<App />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router> */}
+    </Router>
   </React.StrictMode>,
 )

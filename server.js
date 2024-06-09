35fe74aad5bd4fd13175e135ba4f6061bf5c1c0f
daryle <daryle.tan@gmail.com>
2024-06-09 03:40:51 -0500

    commented static

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static("public"))
 
-app.use(express.static(path.join(__dirname, "build")))
+// app.use(express.static(path.join(__dirname, "build")))
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"))

f231b706622b0a7f4aa4bec41b062efdd52f52dd
daryle <daryle.tan@gmail.com>
2024-06-09 01:34:19 -0500

    /*

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>

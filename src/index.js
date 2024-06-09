2772a95644171dd71ede001a7437f9a1a0ad3e06
daryle <daryle.tan@gmail.com>
2024-06-09 01:43:05 -0500

    added gameboard

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App.js"
 import Contact from "./components/Contact.js"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { createRoot } from "react-dom/client"
+import Gameboard from "./components/Gameboard.js"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
@@ -12,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Gameboard />} />
         {/* <Route path="/home" element={<App />} /> */}
         <Route path="/contact" element={<Contact />} />
       </Routes>

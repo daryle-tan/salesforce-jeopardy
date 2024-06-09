94eaf9f6634487cf90ce8882443381edef30bc20
daryle <daryle.tan@gmail.com>
2024-06-09 00:42:10 -0500

    updated 31

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static("public"))
 app.use(express.static(path.join(__dirname, "build")))
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "build/index.html"))
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
 // app.get("/contact", (req, res) => {

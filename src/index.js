30cfa4501f2483174e6e94f0a432257c8f89cb5b
daryle <daryle.tan@gmail.com>
2024-06-09 01:39:09 -0500

    removed App route

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" />
+        {/* <Route path="/home" element={<App />} /> */}
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>

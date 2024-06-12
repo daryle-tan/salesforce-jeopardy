c9155d79a32e6d301de6b9623051be32686f09ac
daryle <daryle.tan@gmail.com>
2024-06-11 21:33:43 -0500

    updated contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,15 +64,6 @@ function Contact() {
         <DesktopView isVisible={isVisible} />
       ) : (
         <div id="contactBackground">
-          <Link className="homeLink" to="/" title="Home Page" target="_blank">
-            <div id="contactNavBar">
-              <img
-                src="/certforcep.webp"
-                alt="Certforce Logo"
-                className="logo"
-              ></img>
-            </div>
-          </Link>
           {contactFormSubmitted ? (
             <div id="confirmationContainer">
               <div className="closeButtonContainer">
@@ -94,82 +85,98 @@ function Contact() {
               </p>
             </div>
           ) : (
-            <div id="contactContainer">
-              <header className="header">
-                <h1 id="title" className="text-center">
-                  Get In Touch
-                </h1>
-                <p id="description" className="text-center">
-                  Thank you for taking the time to reach out!
-                </p>
-              </header>
-              <form id="survey-form" onSubmit={onSubmitContact} ref={formRef}>
-                <div className="form-group">
-                  <label id="name-label">Name: </label>
-                  <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="Wing Bo"
-                    onChange={(event) => setName(event.target.value)}
-                    required
-                  />
+            <>
+              <Link
+                className="homeLink"
+                to="/"
+                title="Home Page"
+                target="_blank"
+              >
+                <div id="contactNavBar">
+                  <img
+                    src="/certforcep.webp"
+                    alt="Certforce Logo"
+                    className="logo"
+                  ></img>
                 </div>
-                <div className="form-group">
-                  <label id="email-label">Email: </label>
-                  <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder="you@email.com"
-                    onChange={(event) => setEmail(event.target.value)}
-                    required
-                  />
-                </div>
-
-                <div className="form-group">
-                  <p className="p-question">
-                    Which option best describes your reason for getting in
-                    touch?
+              </Link>
+              <div id="contactContainer">
+                <header className="header">
+                  <h1 id="title" className="text-center">
+                    Get In Touch
+                  </h1>
+                  <p id="description" className="text-center">
+                    Thank you for taking the time to reach out!
                   </p>
-                  <select
-                    className="dropdown"
-                    name="subject"
-                    onChange={(e) => setSubject(e.target.value)}
-                    required
-                  >
-                    <option value="" disabled selected hidden>
-                      Select your interest
-                    </option>
-                    <option value="Constructive Feedback">
-                      Constructive Feedback
-                    </option>
-                    <option value="Advertise Here">Advertise Here</option>
-                    <option value="Ridiculous Comment">
-                      Ridiculous Comment
-                    </option>
-                    <option value="Other">Other</option>
-                  </select>
-                </div>
+                </header>
+                <form id="survey-form" onSubmit={onSubmitContact} ref={formRef}>
+                  <div className="form-group">
+                    <label id="name-label">Name: </label>
+                    <input
+                      id="name"
+                      type="text"
+                      name="name"
+                      placeholder="Wing Bo"
+                      onChange={(event) => setName(event.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label id="email-label">Email: </label>
+                    <input
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="you@email.com"
+                      onChange={(event) => setEmail(event.target.value)}
+                      required
+                    />
+                  </div>
 
-                <div className="form-group">
-                  <p className="p-comment">Please add your message:</p>
-                  <textarea
-                    id="comments"
-                    className="input-textarea"
-                    name="message"
-                    placeholder="Enter your message here..."
-                    onChange={(event) => setMessage(event.target.value)}
-                    required
-                  ></textarea>
-                </div>
-                <div className="form-button-container">
-                  <button id="submit" type="submit" className="submit-button">
-                    SUBMIT
-                  </button>
-                </div>
-              </form>
-            </div>
+                  <div className="form-group">
+                    <p className="p-question">
+                      Which option best describes your reason for getting in
+                      touch?
+                    </p>
+                    <select
+                      className="dropdown"
+                      name="subject"
+                      onChange={(e) => setSubject(e.target.value)}
+                      required
+                    >
+                      <option value="" disabled selected hidden>
+                        Select your interest
+                      </option>
+                      <option value="Constructive Feedback">
+                        Constructive Feedback
+                      </option>
+                      <option value="Advertise Here">Advertise Here</option>
+                      <option value="Ridiculous Comment">
+                        Ridiculous Comment
+                      </option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+
+                  <div className="form-group">
+                    <p className="p-comment">Please add your message:</p>
+                    <textarea
+                      id="comments"
+                      className="input-textarea"
+                      name="message"
+                      placeholder="Enter your message here..."
+                      onChange={(event) => setMessage(event.target.value)}
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="form-button-container">
+                    <button id="submit" type="submit" className="submit-button">
+                      SUBMIT
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
           )}
         </div>
       )}
